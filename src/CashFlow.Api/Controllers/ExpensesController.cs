19f72cdd7fac2bf1d02cfb848ef939fcfd82fbb6
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2025-06-25 20:59:31 -0300

    Adicionar arquivos de projeto.

---
+++ src/CashFlow.Api/Controllers/ExpensesController.cs
@@ -0,0 +1,45 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExpensesController : ControllerBase
+{
+    [HttpPost]
+    //[ProducesResponseType(typeof(), StatusCodes.Status201Created)]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        var useCase = new RegisterExpensesUseCase();
+
+        var response = useCase.Execute(request);
+
+        return Created(string.Empty, response);
+        /*
+        try
+        {
+            var useCase = new RegisterExpensesUseCase();
+
+            var response = useCase.Execute(request);
+
+            return Created(string.Empty, response);
+        }
+        catch (ErrorOnValidationException ex)
+        {
+            var errorResponse = new ResponseErrorJson(ex.Errors);
+
+            return BadRequest(errorResponse);
+        }
+        catch
+        {
+            var errorResponse = new ResponseErrorJson("Unknown error");
+
+            return StatusCode(StatusCodes.Status500InternalServerError, errorResponse);
+        }
+        */
+    }
+}
---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpensesUseCase.cs
@@ -0,0 +1,57 @@
+﻿using CashFlow.Communication.Enums;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpensesUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        if(result.IsValid == false)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+
+        /*
+         var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+        if (titleIsEmpty) 
+        {
+            throw new ArgumentException("The title is required.");
+        }
+         
+
+        if (request.Amount <= 0)
+        {
+            throw new ArgumentException("The Amount must be greater than zero.");
+        }
+
+        var result = DateTime.Compare(request.Date, DateTime.UtcNow);
+        if (result > 0) 
+        {
+            throw new ArgumentException("Expenses cannot be for the future.");
+        }
+
+        var paymentTypeIsValid = Enum.IsDefined(typeof(PaymentType), request.PaymentType);
+        if (paymentTypeIsValid == false) 
+        {
+            throw new ArgumentException("Payment Type is not valid.");
+        }
+
+        */
+    }
+}
